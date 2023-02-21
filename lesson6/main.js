ce0d1cb9659e050a79b79a19aaa6fc32a59b6a77
Ostapichev <ytox@ukr.net>
2023-02-21 18:28:58 +0200

    HomeWork_JS-lesson6

--- lesson6/main.js
+++ lesson6/main.js
@@ -22,11 +22,13 @@ console.log('');
 //- Є "брудна" стрінга let str = ' dirty string   ' .
 // Почистити її від  пробілів.
 let str = ' dirty string   ';
-console.log(str.trim());
-console.log('');
+let delSpaseStr = str.split(' ')
+    .filter(value => value !== '')
+    .join(' ');
+console.log(delSpaseStr);
 
 //- Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
-let stringToarray = (str) => str.split(' ');
+let stringToarray = str => str.split(' ');
 let str2 = 'Ревуть воли як ясла повні';
 let arr = stringToarray(str2); //['Ревуть', 'воли', 'як', 'ясла', 'повні'];
 console.log(arr);
@@ -70,7 +72,7 @@ console.log(coursesAndDurationArray.sort(
 
 //-- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
 console.log(coursesAndDurationArray.filter(
-    (arr) => arr['monthDuration'] > 5)
+    arr => arr['monthDuration'] > 5)
 );
 
 //-- за допомоги map перетворити кожен елемент на
@@ -83,48 +85,182 @@ console.log(coursesAndDurationArray.map((value, index) => {
           }
     }
 ))
-console.log('');
 
 //=========================
 //описати колоду карт (від 6 до туза без джокерів)
-let cards = [
+let playCards = [
+    {cardSuit: 'spade', value: '6', color: 'black'},
+    {cardSuit: 'spade', value: '7', color: 'black'},
+    {cardSuit: 'spade', value: '8', color: 'black'},
+    {cardSuit: 'spade', value: '9', color: 'black'},
+    {cardSuit: 'spade', value: '10', color: 'black'},
+    {cardSuit: 'spade', value: 'jack', color: 'black'},
+    {cardSuit: 'spade', value: 'queen', color: 'black'},
+    {cardSuit: 'spade', value: 'king', color: 'black'},
+    {cardSuit: 'spade', value: 'ace', color: 'black'},
+
+    {cardSuit: 'clubs', value: '6', color: 'black'},
+    {cardSuit: 'clubs', value: '7', color: 'black'},
+    {cardSuit: 'clubs', value: '8', color: 'black'},
+    {cardSuit: 'clubs', value: '9', color: 'black'},
+    {cardSuit: 'clubs', value: '10', color: 'black'},
+    {cardSuit: 'clubs', value: 'jack', color: 'black'},
+    {cardSuit: 'clubs', value: 'queen', color: 'black'},
+    {cardSuit: 'clubs', value: 'king', color: 'black'},
+    {cardSuit: 'clubs', value: 'ace', color: 'black'},
+
+    {cardSuit: 'diamond', value: '6', color: 'red'},
+    {cardSuit: 'diamond', value: '7', color: 'red'},
+    {cardSuit: 'diamond', value: '8', color: 'red'},
+    {cardSuit: 'diamond', value: '9', color: 'red'},
+    {cardSuit: 'diamond', value: '10', color: 'red'},
+    {cardSuit: 'diamond', value: 'jack', color: 'red'},
+    {cardSuit: 'diamond', value: 'queen', color: 'red'},
+    {cardSuit: 'diamond', value: 'king', color: 'red'},
+    {cardSuit: 'diamond', value: 'ace', color: 'red'},
+
+    {cardSuit: 'heart', value: '6', color: 'red'},
+    {cardSuit: 'heart', value: '7', color: 'red'},
+    {cardSuit: 'heart', value: '8', color: 'red'},
+    {cardSuit: 'heart', value: '9', color: 'red'},
+    {cardSuit: 'heart', value: '10', color: 'red'},
+    {cardSuit: 'heart', value: 'jack', color: 'red'},
+    {cardSuit: 'heart', value: 'queen', color: 'red'},
+    {cardSuit: 'heart', value: 'king', color: 'red'},
+    {cardSuit: 'heart', value: 'ace', color: 'red'}
+];
+
+console.log(playCards, playCards.length);
+
+//- знайти піковий туз
+let spadeAce = playCards.find(value => {
+    return value.cardSuit === 'spade' && value.value === 'ace';
+})
+
+console.log(spadeAce);
+
+//- всі шістки
+let cardSix = playCards.filter(value => {
+    return value.value === '6';
+})
+
+console.log(cardSix);
+
+//- всі червоні карти
+let redCards = playCards.filter(value => {
+    return value.color === 'red';
+})
+
+console.log(redCards);
+
+//- всі буби
+let diamondCards = playCards.filter(value => value.cardSuit === 'diamond');
+console.log(diamondCards);
+
+//- всі трефи від 9 та більше
+
+let clubsCards = playCards
+    .filter(value => value.cardSuit === 'clubs')
+    .slice(3, playCards.length + 1);
+
+console.log(clubsCards);
+
+//=========================
+//    Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
+let cardsBox = playCards.reduce((acc, value) => {
+    if (value.cardSuit === 'spade') acc.spades.push(value);
+    if (value.cardSuit === 'diamond') acc.diamonds.push(value);
+    if (value.cardSuit === 'heart') acc.hearts.push(value);
+    if (value.cardSuit === 'clubs') acc.clubs.push(value);
+    return acc;
+}, {spades:[], diamonds:[], hearts:[], clubs:[]});
+
+console.log(cardsBox)
+
+//=========================
+//  взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+let coursesArray = [
     {
-        cardSuit: ['spade', 'diamond', 'heart', 'clubs'],
-        value: ['6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace'],
-        color: ['red', 'black']
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
     }
 ];
 
-let playCards = cards.map(value => {
-    let playCard = [];
-    for (let item of cards) {
-        for (item of item.cardSuit) {
-            playCard.push({sute: item, value: });
-        }
-    return playCard;
+//--написати пошук всіх об'єктів, в який в modules є sass
+let sass = coursesArray.filter(item => {
+        return item.modules.includes('sass');
     }
-})
+);
 
-console.log(playCards)
+console.log(sass);
 
-//- знайти піковий туз
-//- всі шістки
-//- всі червоні карти
-//- всі буби
-//- всі трефи від 9 та більше
+//--написати пошук всіх об'єктів, в який в modules є docker
+let docker = coursesArray.filter(item => {
+        return item.modules.includes('docker');
+    }
+);
 
-
-
-/*=========================
-
-    Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
-{
-    spades:[],
-        diamonds:[],
-    hearts:[],
-    clubs:[]
-}
-=========================
-    взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
---написати пошук всіх об'єктів, в який в modules є sass
---написати пошук всіх об'єктів, в який в modules є docker*/
+console.log(docker);

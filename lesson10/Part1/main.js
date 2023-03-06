75ab615db1ac39355150cfeba44bf6537f9e3d48
Ostapichev <ytox@ukr.net>
2023-03-06 13:39:57 +0200

    HomeWork_JS-lesson10

--- lesson10/Part1/main.js
+++ lesson10/Part1/main.js
@@ -6,43 +6,52 @@ let User = function (name, surname, age) {
     this.surname = surname;
     this.age = age;
 }
+let homework = document.getElementsByClassName('homework')[0];
+let userBlock = document.getElementsByClassName('users')[0];
+let form = document.forms.form;
 
-let form = document.forms[0];
-let formUser = document.getElementsByClassName('formuser');
-console.log(formUser[0]);
-form.onsubmit = (eo) => {
+form.submit.addEventListener('click', (eo) => {
     eo.preventDefault();
     let user = new User(
         form.username.value,
         form.usersurname.value,
         form.usage.value
     );
-    let userBlock = document.createElement('div');
     let username = document.createElement('p');
-
-    username.innerText = `Name: ${user['name']}. 
+    username.innerText = `Name: ${user['name']}.
                           Surname: ${user['surname']}. 
                           Age: ${user['age']}`;
-    userBlock.appendChild(username);
-    formUser[0].appendChild(userBlock);
-    document.body.append(formUser[0]);
-}
+    userBlock.append(username);
+})
 
 //==========================
 //є сторінка, на якій є блок, в якому знаходиьтся цифра. написати код,
 // який при кожному перезавантажені сторінки буде додавати до неї +1
-let countReload = document.createElement('h2');
-let number = 0;
+let divCount = document.getElementsByClassName('divcount')[0];
+let numCount = document.getElementsByClassName('numcounter')[0];
+let button = document.createElement('button');
 let count = parseInt(localStorage.getItem('count')) || 0;
 count++;
-countReload.innerText = count;
+numCount.innerText = count;
 localStorage.setItem('count', count);
-
-
+button.innerText = 'очистити';
+button.addEventListener('click', () => {
+    localStorage.clear();
+    numCount.innerText = 0;
+})
+divCount.append(button);
 
 //==========================
-/*Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще, в масив sessions зберігається інформація про дату та час відвідування сторінки. Є ще сторінка sessions.html (назва довільна), при відвідуванні якої потрібно відмалювати всю інформацію про відвідування сторінки index.html. Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
-=========================
+//Є сторінка index.html (назва довільна), при відвідуванні якої в локальне сховще,
+//в масив sessions зберігається інформація про дату та час відвідування сторінки.
+//Є ще сторінка sessions.html (назва довільна), при відвідуванні якої потрібно
+//відмалювати всю інформацію про відвідування сторінки index.html.
+//Інфу НЕ виводити в консоль, а побудувати дом структуру під кожну сессію
+localStorage.setItem('sesions', new Date());
+console.log(now);
+
+
+/*=========================
     зробити масив на 100 об'єктів та дві кнопки prev next
 при завантажені сторінки з'являються перші 10 об'єктів.
     При натисканні next виводяться настпні 10 об'єктів
